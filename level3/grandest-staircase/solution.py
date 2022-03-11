b99f749d8f66a28f3af1908545a2aaf49d23fad4
Justin Edwards <justinedwards1230@gmail.com>
2022-03-10 19:04:30 -0500

    passes all test cases

--- level3/grandest-staircase/solution.py
+++ level3/grandest-staircase/solution.py
@@ -1,6 +1,33 @@
 def solution(n):
-    return n
+    print()
+    # base list to track data
+    res = [1]+[0] * n
+    
+    # loop each combination of n
+    for row in range(1, n + 1):
+        print('row', row)
+        print('a', res)
+        
+        # loop backwards from end of row -> middle
+        # computing inner loop down in reverse to row 
+        # allows list to be built from 0 -> n
+        for col in range(n, row - 1, -1):
+            print('col', col, '-', row)
+            res[col] += res[col - row]
+            
+        print('b', res)
+        print()
+    
+    print('c', res) 
+    # single stair height doesnt work. sub 1
+    res[n] -= 1
+    print('d', res)
+    
+    print('\nres', res[n])
+    
+    return res[n]
 
-
-assert(solution(200)) == 487067745
 assert(solution(3)) == 1
+assert(solution(4)) == 1
+assert(solution(5)) == 2
+assert(solution(200)) == 487067745
