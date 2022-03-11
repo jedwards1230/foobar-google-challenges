5b53599d581dcacc508bfddedcb3766cc9204936
Justin Edwards <justinedwards1230@gmail.com>
2022-03-10 19:06:42 -0500

    prepare level 4

---
+++ level4/free-bunny/solution.py
@@ -0,0 +1,11 @@
+def solution(num_buns, num_required):
+    return 1
+
+
+assert(solution(2, 1)) == [[0], [0]]
+assert(solution(4, 4)) == [[0], [1], [2], [3]]
+assert(solution(5, 3)) == [[0, 1, 2, 3, 4, 5], 
+                           [0, 1, 2, 6, 7, 8], 
+                           [0, 3, 4, 6, 7, 9], 
+                           [1, 3, 5, 6, 8, 9], 
+                           [2, 4, 5, 7, 8, 9]]
